0f9d3ab8ae85a1ed100b5ae219bc152a265b8ac8
Joseph Nix <nixjdm@terminallabs.com>
2017-08-03 17:02:40 -0500

    Adding markdown mode, getting rid of old packages list.

--- init.el
+++ init.el
@@ -1,9 +1,6 @@
 ;; require melpa to download web-mode
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 ;; Add .el files to load path
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 ;; Call for package initialization now, and not after this file is finished evaluating
@@ -48,10 +45,46 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (web-mode-edit-element))))
+ '(package-selected-packages (quote (flymd markdown-mode web-mode-edit-element))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-string-face ((t (:foreground "VioletRed3"))))
+ '(font-lock-type-face ((t (:foreground "brightgreen"))))
+ '(web-mode-block-control-face ((t (:inherit nil :foreground "brightcyan"))))
+ '(web-mode-block-delimiter-face ((t (:inherit font-lock-preprocessor-face :foreground "brightcyan"))))
+ '(web-mode-block-face ((t (:background "color-233"))))
+ '(web-mode-current-column-highlight-face ((t (:background "black"))))
+ '(web-mode-html-tag-bracket-face ((t (:foreground "color-246")))))
+
+;; fxbois' theme: see http://web-mode.org plus Joe's tweaks
+
+(set-face-attribute 'default            nil :background "color-233" :foreground "ivory1")
+(set-face-attribute 'fringe             nil :background "gray20")
+(set-face-attribute 'highlight          nil :background "gray16")
+(set-face-attribute 'mode-line          nil :box nil :background "gray26" :foreground "grey50")
+(set-face-attribute 'mode-line-inactive nil :background "gray40")
+(set-face-attribute 'font-lock-function-name-face    nil :foreground "Blue")
+(set-face-attribute 'font-lock-string-face           nil :foreground "VioletRed1")
+
+(set-face-attribute 'web-mode-html-tag-face          nil :foreground "#777777")
+(set-face-attribute 'web-mode-html-tag-custom-face   nil :foreground "#8a9db4")
+(set-face-attribute 'web-mode-html-tag-bracket-face  nil :foreground "gray70")
+(set-face-attribute 'web-mode-html-attr-name-face    nil :foreground "#aaaaaa")
+(set-face-attribute 'web-mode-html-attr-equal-face   nil :foreground "#eeeeee")
+(set-face-attribute 'web-mode-html-attr-value-face   nil :foreground "RosyBrown")
+(set-face-attribute 'web-mode-html-attr-custom-face  nil :foreground "#8a9db4")
+(set-face-attribute 'web-mode-html-attr-engine-face  nil :foreground "#00f5ff")
+(set-face-attribute 'web-mode-comment-face           nil :foreground "Firebrick")
+(set-face-attribute 'web-mode-constant-face          nil :foreground "aquamarine")
+(set-face-attribute 'web-mode-css-at-rule-face       nil :foreground "plum4")
+(set-face-attribute 'web-mode-css-selector-face      nil :foreground "orchid3")
+(set-face-attribute 'web-mode-css-pseudo-class-face  nil :foreground "plum2")
+(set-face-attribute 'web-mode-css-property-name-face nil :foreground "Pink3")
+(set-face-attribute 'web-mode-preprocessor-face      nil :foreground "DarkSeaGreen")
+(set-face-attribute 'web-mode-block-face             nil :background "color-233" :foreground "brightcyan")
+(set-face-attribute 'web-mode-block-control-face     nil :foreground "SeaGreen")
+(set-face-attribute 'web-mode-variable-name-face     nil :foreground "Burlywood")
+(set-face-attribute 'web-mode-function-name-face     nil :foreground "Blue")
