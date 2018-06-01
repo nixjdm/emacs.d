f6f5b20f0b84b3318a67fa79f943e4418274c22b
Joseph Nix <nixjdm@terminallabs.com>
2018-06-01 14:16:05 -0500

    [master] Adding auto pulling of packages if they don't exist. Adding themes. Adding separate custom.el. Using dedicated css and less mode.

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(web-mode salt-mode markdown-mode json-mode flymd monokai-theme cyberpunk-theme color-theme-modern color-theme ample-theme afternoon-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-string-face ((t (:foreground "VioletRed3"))))
+ '(font-lock-type-face ((t (:foreground "brightgreen"))))
+ '(web-mode-block-control-face ((t (:inherit nil :foreground "brightcyan"))))
+ '(web-mode-block-delimiter-face ((t (:inherit font-lock-preprocessor-face :foreground "brightcyan"))))
+ '(web-mode-block-face ((t (:background "color-233"))))
+ '(web-mode-current-column-highlight-face ((t (:background "black"))))
+ '(web-mode-html-tag-bracket-face ((t (:foreground "color-246")))))
--- init.el
+++ init.el
@@ -1,18 +1,54 @@
-;; require melpa to download web-mode
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+;; Set paths to dependencies
+(let ((default-directory
+        (expand-file-name "~/.emacs.d/lisp/")))
+  (normal-top-level-add-subdirs-to-load-path))
+
 (require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-;; Add .el files to load path
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-;; Call for package initialization now, and not after this file is finished evaluating
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 (package-initialize)
-(setq package-enable-at-startup nil)
-;; Configure web-mode work
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+(package-refresh-contents)
+; list the packages you want
+(setq package-list
+      '(
+        ;; themes
+        afternoon-theme
+        ample-theme
+        color-theme
+        color-theme-modern
+        cyberpunk-theme
+        monokai-theme
+        ;; everything else
+        flymd
+        json-mode
+        less-css-mode
+        markdown-mode
+        salt-mode
+        web-mode
+        ))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+;; Configure web-mode
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.template?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.less?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.less?\\'" . less-css-mode))
+(add-to-list 'auto-mode-alist '("\\.css?\\'" . css-mode))
 (setq web-mode-engines-alist
       '(("django"    . "\\.html\\'")))
 (setq web-mode-engines-alist
@@ -42,53 +78,36 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 ;; No tabs
 (setq-default indent-tabs-mode nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (json-mode salt-mode flymd markdown-mode web-mode-edit-element))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(font-lock-string-face ((t (:foreground "VioletRed3"))))
- '(font-lock-type-face ((t (:foreground "brightgreen"))))
- '(web-mode-block-control-face ((t (:inherit nil :foreground "brightcyan"))))
- '(web-mode-block-delimiter-face ((t (:inherit font-lock-preprocessor-face :foreground "brightcyan"))))
- '(web-mode-block-face ((t (:background "color-233"))))
- '(web-mode-current-column-highlight-face ((t (:background "black"))))
- '(web-mode-html-tag-bracket-face ((t (:foreground "color-246")))))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
+;;;;;;;;;;;;;;;;;;;;;
 ;; fxbois' theme: see http://web-mode.org plus Joe's tweaks
 
-(set-face-attribute 'default            nil :background "color-233" :foreground "ivory1")
-(set-face-attribute 'fringe             nil :background "gray20")
-(set-face-attribute 'highlight          nil :background "gray16")
-(set-face-attribute 'mode-line          nil :box nil :background "gray26" :foreground "grey80")
-(set-face-attribute 'mode-line-inactive nil :background "gray40")
-(set-face-attribute 'font-lock-function-name-face    nil :foreground "Blue")
-(set-face-attribute 'font-lock-string-face           nil :foreground "VioletRed1")
+;; (set-face-attribute 'default            nil :background "color-233" :foreground "ivory1")
+;; (set-face-attribute 'fringe             nil :background "gray20")
+;; (set-face-attribute 'highlight          nil :background "gray16")
+;; (set-face-attribute 'mode-line          nil :box nil :background "gray26" :foreground "grey80")
+;; (set-face-attribute 'mode-line-inactive nil :background "gray40")
+;; (set-face-attribute 'font-lock-function-name-face    nil :foreground "Blue")
+;; (set-face-attribute 'font-lock-string-face           nil :foreground "VioletRed1")
 
-(set-face-attribute 'web-mode-html-tag-face          nil :foreground "#777777")
-(set-face-attribute 'web-mode-html-tag-custom-face   nil :foreground "#8a9db4")
-(set-face-attribute 'web-mode-html-tag-bracket-face  nil :foreground "gray70")
-(set-face-attribute 'web-mode-html-attr-name-face    nil :foreground "#aaaaaa")
-(set-face-attribute 'web-mode-html-attr-equal-face   nil :foreground "#eeeeee")
-(set-face-attribute 'web-mode-html-attr-value-face   nil :foreground "RosyBrown")
-(set-face-attribute 'web-mode-html-attr-custom-face  nil :foreground "#8a9db4")
-(set-face-attribute 'web-mode-html-attr-engine-face  nil :foreground "#00f5ff")
-(set-face-attribute 'web-mode-comment-face           nil :foreground "Firebrick")
-(set-face-attribute 'web-mode-constant-face          nil :foreground "aquamarine")
-(set-face-attribute 'web-mode-css-at-rule-face       nil :foreground "plum4")
-(set-face-attribute 'web-mode-css-selector-face      nil :foreground "orchid3")
-(set-face-attribute 'web-mode-css-pseudo-class-face  nil :foreground "plum2")
-(set-face-attribute 'web-mode-css-property-name-face nil :foreground "Pink3")
-(set-face-attribute 'web-mode-preprocessor-face      nil :foreground "DarkSeaGreen")
-(set-face-attribute 'web-mode-block-face             nil :background "color-233" :foreground "brightcyan")
-(set-face-attribute 'web-mode-block-control-face     nil :foreground "SeaGreen")
-(set-face-attribute 'web-mode-variable-name-face     nil :foreground "Burlywood")
-(set-face-attribute 'web-mode-function-name-face     nil :foreground "Blue")
+;; (set-face-attribute 'web-mode-html-tag-face          nil :foreground "#777777")
+;; (set-face-attribute 'web-mode-html-tag-custom-face   nil :foreground "#8a9db4")
+;; (set-face-attribute 'web-mode-html-tag-bracket-face  nil :foreground "gray70")
+;; (set-face-attribute 'web-mode-html-attr-name-face    nil :foreground "#aaaaaa")
+;; (set-face-attribute 'web-mode-html-attr-equal-face   nil :foreground "#eeeeee")
+;; (set-face-attribute 'web-mode-html-attr-value-face   nil :foreground "RosyBrown")
+;; (set-face-attribute 'web-mode-html-attr-custom-face  nil :foreground "#8a9db4")
+;; (set-face-attribute 'web-mode-html-attr-engine-face  nil :foreground "#00f5ff")
+;; (set-face-attribute 'web-mode-comment-face           nil :foreground "Firebrick")
+;; (set-face-attribute 'web-mode-constant-face          nil :foreground "aquamarine")
+;; (set-face-attribute 'web-mode-css-at-rule-face       nil :foreground "plum4")
+;; (set-face-attribute 'web-mode-css-selector-face      nil :foreground "orchid3")
+;; (set-face-attribute 'web-mode-css-pseudo-class-face  nil :foreground "plum2")
+;; (set-face-attribute 'web-mode-css-property-name-face nil :foreground "Pink3")
+;; (set-face-attribute 'web-mode-preprocessor-face      nil :foreground "DarkSeaGreen")
+;; (set-face-attribute 'web-mode-block-face             nil :background "color-233" :foreground "brightcyan")
+;; (set-face-attribute 'web-mode-block-control-face     nil :foreground "SeaGreen")
+;; (set-face-attribute 'web-mode-variable-name-face     nil :foreground "Burlywood")
+;; (set-face-attribute 'web-mode-function-name-face     nil :foreground "Blue")
