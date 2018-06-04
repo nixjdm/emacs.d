922e6b6a1d356541d5db160653ef4324ef13ffaa
Joseph Nix <nixjdm@terminallabs.com>
2018-06-04 16:07:11 -0500

    [master] various package updates.

---
+++ elpa/klere-theme-20180415.1123/klere-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "klere-theme" "20180415.1123" "A dark theme with lambent color highlights and incremental grays" '((emacs "24")) :commit "8e389530dd783dcaab2365e0970181c72d782efb" :url "https://github.com/WammKD/emacs-klere-theme")
---
+++ elpa/nimbus-theme-20180602.731/nimbus-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "nimbus-theme" "20180602.731" "An awesome dark theme" 'nil :commit "9f6c377372ed8bf781401c06c863b64346317246" :keywords '("faces") :url "https://github.com/m-cat/nimbus-theme")
