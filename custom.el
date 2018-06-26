722fcbda8b6c2eb7d0e8825d456ad2fc37ae796e
smitty42 <smith@terminallabs.com>
2018-06-26 14:49:10 -0500

    pushing custon for good measure

--- custom.el
+++ custom.el
@@ -9,29 +9,33 @@
    ["#454545" "#d65946" "#6aaf50" "#baba36" "#5180b3" "#ab75c3" "#68a5e9" "#bdbdb3"])
  '(ansi-term-color-vector
    [unspecified "#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#6c71c4" "#268bd2" "#93a1a1"])
- '(compilation-message-face 'default)
+ '(compilation-message-face (quote default))
  '(custom-safe-themes
-   '("b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "551596f9165514c617c99ad6ce13196d6e7caa7035cea92a0e143dbe7b28be0e" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" "405ccc1240dbc603a476d1d30f5207654e909f1d7e44ba66431b765464c6b3f4" "f41ecd2c34a9347aeec0a187a87f9668fa8efb843b2606b6d5d92a653abe2439" "779ed07226c184f2be8d90a9ca57bae2861e9a17b0bac482360bcee07cf3b862" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "a455366c5cdacebd8adaa99d50e37430b0170326e7640a688e9d9ad406e2edfd" "0f302165235625ca5a827ac2f963c102a635f27879637d9021c04d845a32c568" default))
+   (quote
+    ("b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "551596f9165514c617c99ad6ce13196d6e7caa7035cea92a0e143dbe7b28be0e" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" "405ccc1240dbc603a476d1d30f5207654e909f1d7e44ba66431b765464c6b3f4" "f41ecd2c34a9347aeec0a187a87f9668fa8efb843b2606b6d5d92a653abe2439" "779ed07226c184f2be8d90a9ca57bae2861e9a17b0bac482360bcee07cf3b862" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "a455366c5cdacebd8adaa99d50e37430b0170326e7640a688e9d9ad406e2edfd" "0f302165235625ca5a827ac2f963c102a635f27879637d9021c04d845a32c568" default)))
  '(cycle-themes-mode t)
  '(fci-rule-color "#3C3D37")
- '(highlight-changes-colors '("#FD5FF0" "#AE81FF"))
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
  '(highlight-tail-colors
-   '(("#3C3D37" . 0)
+   (quote
+    (("#3C3D37" . 0)
      ("#679A01" . 20)
      ("#4BBEAE" . 30)
      ("#1DB4D0" . 50)
      ("#9A8F21" . 60)
      ("#A75B00" . 70)
      ("#F309DF" . 85)
-     ("#3C3D37" . 100)))
+     ("#3C3D37" . 100))))
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
-   '(smooth-scrolling smooth-scrolling-mode company-quickhelp themes-list nimbus-theme klere-theme blacken cycle-themes web-mode salt-mode markdown-mode json-mode flymd monokai-theme cyberpunk-theme color-theme-modern color-theme ample-theme afternoon-theme))
+   (quote
+    (smooth-scrolling smooth-scrolling-mode company-quickhelp themes-list nimbus-theme klere-theme blacken cycle-themes web-mode salt-mode markdown-mode json-mode flymd monokai-theme cyberpunk-theme color-theme-modern color-theme ample-theme afternoon-theme)))
  '(pos-tip-background-color "#FFFACE")
  '(pos-tip-foreground-color "#272822")
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
-   '((20 . "#F92672")
+   (quote
+    ((20 . "#F92672")
      (40 . "#CF4F1F")
      (60 . "#C26C0F")
      (80 . "#E6DB74")
@@ -48,10 +52,11 @@
      (300 . "#299BA6")
      (320 . "#2896B5")
      (340 . "#2790C3")
-     (360 . "#66D9EF")))
+     (360 . "#66D9EF"))))
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
-   '(unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
+   (quote
+    (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
